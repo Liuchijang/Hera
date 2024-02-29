c7a4c313eda82c6adc5ad6a78e4e744eef8e6757
Liuchijang <truonggiang20002@gmail.com>
2024-02-29 22:00:37 +0700

    fix old code (Khoa)

--- event_log.py
+++ event_log.py
@@ -3,15 +3,16 @@ import re
 import json
 from velociraptor_sever_api import Run_velociraptor_query
 
-def network_module():
-    Artifact = "Windows.Network.NetstatEnriched"
-    query = "Select * from Artifact.Windows.Network.NetstatEnriched()"
+
+def event_log_module():
+    Artifact = "Windows.EventLogs.LocalHayabusa"
+    query = "select * from Artifact.Windows.EventLogs.LocalHayabusa()"
     output = Run_velociraptor_query(query)
     correctSyntax = re.sub(r"\]\[", ",",output)
     parsed = json.loads(correctSyntax)
 
     # Optionally write output to file
     cwd = os.getcwd()
-    filepath = os.path.join(cwd,"network-activities-of-processes.json")
+    filepath = os.path.join(cwd,"Event log module's output.json")
     with open(filepath, 'wb') as f:
         f.write(output.encode('utf8', 'ignore'))
