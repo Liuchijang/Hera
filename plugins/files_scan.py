38bb0839905f888e3aa725c69be9ea9467a6ec6b
Liuchijang <truonggiang20002@gmail.com>
2024-03-06 20:43:17 +0700

    add check virustotal

--- plugins/files_scan.py
+++ plugins/files_scan.py
@@ -1,5 +1,6 @@
 import re
 from core.velociraptor_sever_api import Run_velociraptor_query
+from core.check_virustotal import check_virustotal
 
 def fileScan_module():
     artifact = "Files.scan"
@@ -7,8 +8,13 @@ def fileScan_module():
     output = Run_velociraptor_query(query)
     correctSyntax = re.sub(r"\]\[", ",",output)
     parsed = eval(correctSyntax)
-    print(parsed)
-    return parsed
+    result = []
+    for i in parsed:
+        check = check_virustotal("check_hash", i['MD5'])
+        if "Malicious" in check:
+            print(i['OSPath'])
+            result.extend(i)
+    return result
 
 
 if __name__ == "__main__":
