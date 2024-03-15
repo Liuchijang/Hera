ce71ecc7e0020b693a002b3378ac1c70d1501796
v0dk4-102 <nguyenvanan01012000bt@gmail.com>
2024-03-15 15:56:59 +0700

    fix save option

--- plugins/files_scan.py
+++ plugins/files_scan.py
@@ -1,9 +1,10 @@
 import os
 import re
+import json
 from core.velociraptor_sever_api import Run_velociraptor_query
 from core.check_virustotal import *
 
-def fileScan_module(outputFolder, save_to_file=False):
+def fileScan_module(outputFolder, verbose=False,save_to_file=False):
     print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\nFiles scanning...")
     artifact = "Files.scan"
     query = "select * from Artifact.{}()".format(artifact)
@@ -15,19 +16,21 @@ def fileScan_module(outputFolder, save_to_file=False):
         for i in parsed:
             check = check_virustotal("check_hash", i['MD5'])
             if check == 1:
-                print("File Path: " + i['OSPath'])
+                if verbose: print("Detected malicious file, FilePath: " + i['OSPath'])
                 result.extend(i)
     else:
         with open("./data/malicious_MD5.txt", "r") as f:
             malicious_MD5 = set(line.strip() for line in f)
         for i in parsed:
             if i['MD5'] in malicious_MD5:
-                print("File Path: " + i['OSPath'])
+                if verbose: print("Detected malicious file, FilePath: " + i['OSPath'])
                 result.extend(i)
     if save_to_file:
         filepath = os.path.join(outputFolder,"Files_module.json")
-        with open(filepath, 'wb') as f:
-            f.write(result.encode('utf8', 'ignore'))
+        with open(filepath, 'w') as f:
+            json.dump(result,f,indent=4)
+            print(f"Saved file module output at {filepath}")
+    print("Scan Files completed.")
     return result
     
 
