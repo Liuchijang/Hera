2dbabeab20fa2d1709e56adf7a8577e56545b6e8
Liuchijang <truonggiang20002@gmail.com>
2024-03-18 13:47:25 +0700

    add match funtion to main

--- core/matching.py
+++ core/matching.py
@@ -11,34 +11,36 @@ with open(filepath,"r") as file:
     for line in file:
         event_log.append(json.loads(line))
 
+lst = []
+process_tree = []
+for i in event_log:
+    if i['EventID'] == 4688 and 'ExtraFieldInfo' in i and 'ParentProcessName' in i['ExtraFieldInfo']:
+        lst.append(i)
+# print(event_log[0]['Timestamp'])
+pid_to_ppid = defaultdict(list)
+pid_to_cmd = defaultdict(list)
+ppid_to_name = defaultdict(list)
+pid_to_name = defaultdict(list)
+# Đổ dữ liệu từ danh sách vào defaultdict
+for item in lst:
+    pid = item['Details']['PID']
+    ppid = item['ExtraFieldInfo']['ProcessId']
+    lid = item["Details"]['LID']
+    cmd = item['Details']['Cmdline']
+    pid_to_ppid[(ppid, lid)].append((pid, lid))
+    pid_to_cmd[(pid, lid)] = (cmd, lid)
+    pid_to_name[(pid, lid)] = (item['Details']['Proc'], lid)
+    if 'ExtraFieldInfo' in item and 'ParentProcessName' in item['ExtraFieldInfo']:
+        ppid_to_name[(ppid, lid)] = (item['ExtraFieldInfo']['ParentProcessName'], lid)
+    else: ppid_to_name[(ppid, lid)] = ("", lid)
+
+
 def create_process_tree():
-    lst = []
-    process_tree = []
-    for i in event_log:
-        if i['EventID'] == 4688:
-            lst.append(i)
-
-    # print(event_log[0]['Timestamp'])
-    pid_to_ppid = defaultdict(list)
-    pid_to_cmd = defaultdict(list)
-    ppid_to_name = defaultdict(list)
-
-    # Đổ dữ liệu từ danh sách vào defaultdict
-    for item in lst:
-        pid = item['Details']['PID']
-        ppid = item['ExtraFieldInfo']['ProcessId']
-        lid = item["Details"]['LID']
-        cmd = item['Details']['Cmdline']
-        pid_to_ppid[(ppid, lid)].append((pid, lid))
-        pid_to_cmd[(pid, lid)] = (cmd, lid)
-        if 'ExtraFieldInfo' in item and 'ParentProcessName' in item['ExtraFieldInfo']:
-            ppid_to_name[(ppid, lid)] = (item['ExtraFieldInfo']['ParentProcessName'], lid)
-        else: ppid_to_name[(ppid, lid)] = ("", lid)
-
     # Hiển thị cây quan hệ giữa PID và ProcessId
     def display_tree(ppid, pid_dict, prefix='', is_first=True, is_last=True):
         pid_dict[ppid] = pid_to_cmd[ppid]   
         if is_last and is_first:
+            print()
             print(prefix + '└── ' + f"{ppid[0]} - {ppid_to_name[ppid][0]}")
         elif is_last and not is_first:
             print(prefix + '└── ' + f"{ppid[0]} - {pid_to_cmd[ppid][0]}")    
@@ -66,7 +68,15 @@ def creat_object(process_tree):
     for i in process_tree:
         malware_instances.append(Malware(i))
 
-if __name__ == "__main__":
+def matching(process, registry, files, network, wmi):
+    print(process)
+    print(registry)
+    print(files)
+    print(network)
+    print(wmi)
     creat_object(create_process_tree())
+
+if __name__ == "__main__":
     # for i in malware_instances:
     #     print(i.process)
+    print("Done")
